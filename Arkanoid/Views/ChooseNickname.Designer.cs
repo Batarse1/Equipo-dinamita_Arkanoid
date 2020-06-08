d523e110cb2f29c0fd17059467ff2f832e1fb6f3
RIADAPLE <00032219@uca.edu.sv>
2020-06-07 18:59:19 -0600

    Añadiendo las instrucciones

--- Arkanoid/Views/ChooseNickname.Designer.cs
+++ Arkanoid/Views/ChooseNickname.Designer.cs
@@ -31,7 +31,8 @@ namespace Arkanoid
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ChooseNickname));
+            System.ComponentModel.ComponentResourceManager resources =
+                new System.ComponentModel.ComponentResourceManager(typeof(ChooseNickname));
             this.tableLayoutPanelChooseNickname = new System.Windows.Forms.TableLayoutPanel();
             this.lblAskForNickname = new System.Windows.Forms.Label();
             this.btnReturn = new System.Windows.Forms.Button();
@@ -46,11 +47,16 @@ namespace Arkanoid
             // tableLayoutPanelChooseNickname
             // 
             this.tableLayoutPanelChooseNickname.ColumnCount = 5;
-            this.tableLayoutPanelChooseNickname.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5F));
-            this.tableLayoutPanelChooseNickname.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
-            this.tableLayoutPanelChooseNickname.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
-            this.tableLayoutPanelChooseNickname.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
-            this.tableLayoutPanelChooseNickname.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanelChooseNickname.ColumnStyles.Add(
+                new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanelChooseNickname.ColumnStyles.Add(
+                new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanelChooseNickname.ColumnStyles.Add(
+                new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanelChooseNickname.ColumnStyles.Add(
+                new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanelChooseNickname.ColumnStyles.Add(
+                new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5F));
             this.tableLayoutPanelChooseNickname.Controls.Add(this.lblAskForNickname, 1, 1);
             this.tableLayoutPanelChooseNickname.Controls.Add(this.btnReturn, 1, 5);
             this.tableLayoutPanelChooseNickname.Controls.Add(this.pictureBox1, 2, 2);
@@ -59,27 +65,36 @@ namespace Arkanoid
             this.tableLayoutPanelChooseNickname.Controls.Add(this.lblNickname, 3, 3);
             this.tableLayoutPanelChooseNickname.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanelChooseNickname.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanelChooseNickname.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tableLayoutPanelChooseNickname.Name = "tableLayoutPanelChooseNickname";
             this.tableLayoutPanelChooseNickname.RowCount = 7;
-            this.tableLayoutPanelChooseNickname.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
-            this.tableLayoutPanelChooseNickname.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
-            this.tableLayoutPanelChooseNickname.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
-            this.tableLayoutPanelChooseNickname.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
-            this.tableLayoutPanelChooseNickname.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 47.5F));
-            this.tableLayoutPanelChooseNickname.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
-            this.tableLayoutPanelChooseNickname.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
-            this.tableLayoutPanelChooseNickname.Size = new System.Drawing.Size(882, 503);
+            this.tableLayoutPanelChooseNickname.RowStyles.Add(
+                new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanelChooseNickname.RowStyles.Add(
+                new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanelChooseNickname.RowStyles.Add(
+                new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanelChooseNickname.RowStyles.Add(
+                new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanelChooseNickname.RowStyles.Add(
+                new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 47.5F));
+            this.tableLayoutPanelChooseNickname.RowStyles.Add(
+                new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanelChooseNickname.RowStyles.Add(
+                new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanelChooseNickname.Size = new System.Drawing.Size(772, 472);
             this.tableLayoutPanelChooseNickname.TabIndex = 0;
             // 
             // lblAskForNickname
             // 
             this.lblAskForNickname.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lblAskForNickname.Font = new System.Drawing.Font("Super Mario 256", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.lblAskForNickname.Font = new System.Drawing.Font("Super Mario 256", 18F, System.Drawing.FontStyle.Bold,
+                System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.lblAskForNickname.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.lblAskForNickname.Location = new System.Drawing.Point(47, 28);
-            this.lblAskForNickname.Margin = new System.Windows.Forms.Padding(3);
+            this.lblAskForNickname.Location = new System.Drawing.Point(40, 25);
+            this.lblAskForNickname.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.lblAskForNickname.Name = "lblAskForNickname";
-            this.lblAskForNickname.Size = new System.Drawing.Size(258, 56);
+            this.lblAskForNickname.Size = new System.Drawing.Size(227, 55);
             this.lblAskForNickname.TabIndex = 1;
             this.lblAskForNickname.Text = "ALIAS";
             this.lblAskForNickname.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -90,11 +105,12 @@ namespace Arkanoid
             this.btnReturn.Dock = System.Windows.Forms.DockStyle.Fill;
             this.btnReturn.FlatAppearance.BorderColor = System.Drawing.Color.White;
             this.btnReturn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnReturn.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
-            this.btnReturn.Location = new System.Drawing.Point(55, 423);
-            this.btnReturn.Margin = new System.Windows.Forms.Padding(11);
+            this.btnReturn.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold,
+                System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.btnReturn.Location = new System.Drawing.Point(47, 398);
+            this.btnReturn.Margin = new System.Windows.Forms.Padding(9, 10, 9, 10);
             this.btnReturn.Name = "btnReturn";
-            this.btnReturn.Size = new System.Drawing.Size(242, 40);
+            this.btnReturn.Size = new System.Drawing.Size(213, 39);
             this.btnReturn.TabIndex = 6;
             this.btnReturn.Text = "VOLVER";
             this.btnReturn.UseVisualStyleBackColor = false;
@@ -102,13 +118,15 @@ namespace Arkanoid
             // 
             // pictureBox1
             // 
-            this.pictureBox1.BackgroundImage = ((System.Drawing.Image) (resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImage =
+                ((System.Drawing.Image) (resources.GetObject("pictureBox1.BackgroundImage")));
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox1.Location = new System.Drawing.Point(311, 90);
+            this.pictureBox1.Location = new System.Drawing.Point(271, 84);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
             this.tableLayoutPanelChooseNickname.SetRowSpan(this.pictureBox1, 4);
-            this.pictureBox1.Size = new System.Drawing.Size(258, 381);
+            this.pictureBox1.Size = new System.Drawing.Size(227, 361);
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
             // 
@@ -118,11 +136,12 @@ namespace Arkanoid
             this.btnPlay.Dock = System.Windows.Forms.DockStyle.Fill;
             this.btnPlay.FlatAppearance.BorderColor = System.Drawing.Color.White;
             this.btnPlay.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnPlay.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
-            this.btnPlay.Location = new System.Drawing.Point(583, 423);
-            this.btnPlay.Margin = new System.Windows.Forms.Padding(11);
+            this.btnPlay.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold,
+                System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.btnPlay.Location = new System.Drawing.Point(509, 398);
+            this.btnPlay.Margin = new System.Windows.Forms.Padding(9, 10, 9, 10);
             this.btnPlay.Name = "btnPlay";
-            this.btnPlay.Size = new System.Drawing.Size(242, 40);
+            this.btnPlay.Size = new System.Drawing.Size(213, 39);
             this.btnPlay.TabIndex = 5;
             this.btnPlay.Text = "JUGAR";
             this.btnPlay.UseVisualStyleBackColor = false;
@@ -130,12 +149,16 @@ namespace Arkanoid
             // 
             // txtNickname
             // 
-            this.txtNickname.Anchor = ((System.Windows.Forms.AnchorStyles) ((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtNickname.Anchor =
+                ((System.Windows.Forms.AnchorStyles) ((System.Windows.Forms.AnchorStyles.Left |
+                                                       System.Windows.Forms.AnchorStyles.Right)));
             this.tableLayoutPanelChooseNickname.SetColumnSpan(this.txtNickname, 2);
-            this.txtNickname.Font = new System.Drawing.Font("Super Mario 256", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
-            this.txtNickname.Location = new System.Drawing.Point(311, 37);
+            this.txtNickname.Font = new System.Drawing.Font("Super Mario 256", 18F, System.Drawing.FontStyle.Bold,
+                System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.txtNickname.Location = new System.Drawing.Point(271, 37);
+            this.txtNickname.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtNickname.Name = "txtNickname";
-            this.txtNickname.Size = new System.Drawing.Size(522, 37);
+            this.txtNickname.Size = new System.Drawing.Size(458, 31);
             this.txtNickname.TabIndex = 2;
             this.txtNickname.TextChanged += new System.EventHandler(this.nickname_TextChanged);
             // 
@@ -143,23 +166,25 @@ namespace Arkanoid
             // 
             this.lblNickname.BackColor = System.Drawing.Color.Firebrick;
             this.lblNickname.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lblNickname.Font = new System.Drawing.Font("Super Mario 256", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.lblNickname.Font = new System.Drawing.Font("Super Mario 256", 24F, System.Drawing.FontStyle.Bold,
+                System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.lblNickname.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.lblNickname.Location = new System.Drawing.Point(575, 115);
-            this.lblNickname.Margin = new System.Windows.Forms.Padding(3);
+            this.lblNickname.Location = new System.Drawing.Point(502, 107);
+            this.lblNickname.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.lblNickname.Name = "lblNickname";
-            this.lblNickname.Size = new System.Drawing.Size(258, 56);
+            this.lblNickname.Size = new System.Drawing.Size(227, 55);
             this.lblNickname.TabIndex = 4;
             this.lblNickname.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // ChooseNickname
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Firebrick;
-            this.ClientSize = new System.Drawing.Size(882, 503);
+            this.ClientSize = new System.Drawing.Size(772, 472);
             this.Controls.Add(this.tableLayoutPanelChooseNickname);
             this.Icon = ((System.Drawing.Icon) (resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "ChooseNickname";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Arkanoid By Ribal";
--- Arkanoid/Views/Game.Designer.cs
+++ Arkanoid/Views/Game.Designer.cs
@@ -31,34 +31,44 @@ namespace Arkanoid
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Game));
+            System.ComponentModel.ComponentResourceManager resources =
+                new System.ComponentModel.ComponentResourceManager(typeof(Game));
             this.tableLayoutPanelGame = new System.Windows.Forms.TableLayoutPanel();
             this.SuspendLayout();
             // 
             // tableLayoutPanelGame
             // 
             this.tableLayoutPanelGame.ColumnCount = 3;
-            this.tableLayoutPanelGame.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanelGame.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanelGame.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanelGame.ColumnStyles.Add(
+                new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanelGame.ColumnStyles.Add(
+                new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanelGame.ColumnStyles.Add(
+                new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.tableLayoutPanelGame.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanelGame.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanelGame.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tableLayoutPanelGame.Name = "tableLayoutPanelGame";
             this.tableLayoutPanelGame.RowCount = 4;
-            this.tableLayoutPanelGame.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
-            this.tableLayoutPanelGame.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 55.55556F));
-            this.tableLayoutPanelGame.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 22.22222F));
-            this.tableLayoutPanelGame.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
-            this.tableLayoutPanelGame.Size = new System.Drawing.Size(882, 503);
+            this.tableLayoutPanelGame.RowStyles.Add(
+                new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanelGame.RowStyles.Add(
+                new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 55.55556F));
+            this.tableLayoutPanelGame.RowStyles.Add(
+                new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 22.22222F));
+            this.tableLayoutPanelGame.RowStyles.Add(
+                new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanelGame.Size = new System.Drawing.Size(772, 472);
             this.tableLayoutPanelGame.TabIndex = 1;
-            this.tableLayoutPanelGame.MouseMove += new System.Windows.Forms.MouseEventHandler(this.tableLayoutPanelGame_MouseMove);
+            this.tableLayoutPanelGame.MouseMove +=
+                new System.Windows.Forms.MouseEventHandler(this.tableLayoutPanelGame_MouseMove);
             // 
             // Game
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightSkyBlue;
-            this.ClientSize = new System.Drawing.Size(882, 503);
+            this.ClientSize = new System.Drawing.Size(772, 472);
             this.Controls.Add(this.tableLayoutPanelGame);
             this.Icon = ((System.Drawing.Icon) (resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(2);
